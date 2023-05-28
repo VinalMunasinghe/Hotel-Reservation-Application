8dc8cf08a7006c57d333968a467987988f5403c2
Vinal Munasinghe <87310632+IT21062988@users.noreply.github.com>
2023-05-28 13:41:45 +0530

    Model

--- api/models/Hotel.js
+++ api/models/Hotel.js
@@ -48,3 +48,5 @@ const hotelSchema = new mongoose.Schema({
 
 
 });
+
+export default mongoose.model("Hotel", hotelSchema);
--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -3,7 +3,16 @@ import express from "express";
 const router = express.Router();
 
 //CREATE
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
+
+    const newHotel = new Hotel(req.body);
+
+
+    try {
+        
+    } catch (error) {
+        res.status(500).json(err)
+    }
     
 });
 //DELETE
