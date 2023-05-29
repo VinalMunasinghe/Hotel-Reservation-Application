9f769e41878f335790524d957966b10b71199f49
Vinal Munasinghe <87310632+IT21062988@users.noreply.github.com>
2023-05-29 20:19:16 +0530

    error

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,5 +1,6 @@
 import express from "express";
 import Hotel from "../models/Hotel.js";
+import { CreateError } from "../utils/error.js";
 
 const router = express.Router();
 
@@ -54,14 +55,8 @@ router.get("/:id", async (req, res) => {
 //GET ALL
 router.get("/", async (req, res,next) => {
 
-     const failed = true
-     const err=new Error()
-     err.status = 404
-     err.message="Not Found"
-     if (failed) return next(err)
-
     try {
-       const hotels = await Hotel.findById("efsghfdhjcgj");
+       const hotels = await Hotel.find();
        res.status(200).json(hotels);
    } catch (error) {
        next(error)
--- api/utils/error.js
+++ api/utils/error.js
@@ -1,5 +1,6 @@
 export const CreateError = (status, message) => {
     const err=new Error()
-    err.status = 404
-    err.message="Not Found"
+    err.status = status
+    err.message=message
+    return err;
 }
