a0953d13fb1bac85031bb7647b8ad6421c3bbf17
Vinal Munasinghe <87310632+IT21062988@users.noreply.github.com>
2023-05-30 19:34:52 +0530

    Update hotel.js

--- api/controllers/hotel.js
+++ api/controllers/hotel.js
@@ -1,3 +1,5 @@
+import Hotel from "../models/Hotel.js";
+
 export const createHotel = async (req, res) => {
     const newHotel = new Hotel(req.body);
 
@@ -6,6 +8,46 @@ export const createHotel = async (req, res) => {
         res.status(200).json(savedHotel);
     } catch (error) {
         res.status(500).json(err)
+        next (err)
+    }
+}
+
+export const updatedHotel = async (req, res) => {
+      try {
+        const updatedHotel = await Hotel.findByIdAndUpdate(
+            req.params.id,
+            { $set: req.body}, 
+            {new: true}
+            )
+        res.status(200).json(updatedHotel);
+    } catch (error) {
+        next (err)
+    }
+}
+
+export const deleteHotel = async (req, res) => {
+    try {
+        const deleteHotel = await Hotel.findByIdAndDelete(req.params.id)
+        res.status(200).json("Hotel has been deleted...");
+    } catch (error) {
+        next (err)
+    }
+}
+
+export const getHotel = async (req, res) => {
+    try {
+        const hotel = await Hotel.findById(req.params.id)
+        res.status(200).json(hotel);
+    } catch (error) {
+        next (err)
+    }
+}
+
+export const getHotels = async (req, res) => {
+    try {
+        const hotels = await Hotel.find();
+        res.status(200).json(hotels);
+    } catch (error) {
         next (err)
     }
 }
