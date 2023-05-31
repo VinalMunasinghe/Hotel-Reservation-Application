ba4b4abcf5a6fcb7cdfca33cf44d03d1c7fbbc47
Vinal Munasinghe <87310632+IT21062988@users.noreply.github.com>
2023-05-31 14:08:16 +0530

    Auth

--- api/models/User.js
+++ api/models/User.js
@@ -2,51 +2,26 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const UserSchema = new mongoose.Schema({
-    name:{
+    username:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
         type: String,
         required: true
     },
-    type:{
-        type: String,
-        required: true
-    },
-    city:{
-        type: String,
-        required: true
-    },
-    address:{
-        type: String,
-        required: true
-    },
-    distance:{
-        type: String,
-        required: true
-    },
-    title:{
-        type: [String]
-    },
-    desc:{
-        type: String,
-        required: true
-    },
-    rating:{
-        type: Number,
-        min: 0,
-        max: 5,
-    },
-    rooms:{
-        type: [String],
-    },
-    cheapestPrice:{
-        type: Number,
-        required: true
-    },
-    featured:{
+    isAdmin:{
         type: Boolean,
         required: false
     },
-
-
-});
+},
+ {timestamps: true}
+    );
 
 export default mongoose.model("User", UserSchema);
