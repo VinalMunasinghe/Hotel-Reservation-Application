a57c508d2a24316d95cb96b8e16142d2705e7da6
Vinal Munasinghe <87310632+IT21062988@users.noreply.github.com>
2023-06-02 02:37:15 +0530

    Update auth.js

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js"
 import bcrypt from "bcryptjs"
 import createError from "http-errors"
+import jwt from "jsonwebtoken"
 
 export const register = async (req, res, next) => {
     try {
@@ -34,6 +35,8 @@ export const login = async (req, res, next) => {
 
          if(!isPasswordCorrect)
         return next (createError (400, "Password is not correct"))
+
+        const token = jwt.sign({id: user._id, isAdmin: user.isAdmin},"edsggsfdgh")
        
         const {password, isAdmin, ...otherDetails    } = user._doc;
         res.status(200).json({...otherDetails});
