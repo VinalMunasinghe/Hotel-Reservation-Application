73d3f5f32e7d53b3b4ae8c52cd589549605a57be
Vinal Munasinghe <87310632+IT21062988@users.noreply.github.com>
2023-06-01 20:03:32 +0530

    Update auth.js

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -23,7 +23,6 @@ export const register = async (req, res, next) => {
 }
 
 export const login = async (req, res, next) => {
-    console.log(req.body)
     try {
         const user = await User.findOne({username: req.body.username})
         if(!user) return next (createError(404, "User not found"))
